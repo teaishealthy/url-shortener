506c87f7172629f659b72a753df211e846e93459
teaishealthy <teaishealthy@protonmail.com>
2021-12-05 07:21:13 +0100

    simplify connection

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,28 @@
-import sqlite3
 import re
-
-from .bootstrap import get_base_folder_path
 
 from flask import Flask, redirect
 from flask_cors import CORS
 
+if __name__ != '__main__':
+    from .bootstrap import get_base_folder_path, connection
+else:
+    from bootstrap import get_base_folder_path, connection
+
 
 app = Flask(__name__)
 CORS(app)
+
 pattern = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
 with open(f'{get_base_folder_path()}/../frontend/index.html', 'r') as f:
     index_data = f.read()
+
 @app.post('/short/<path:url>')
 def create_short(url: str):
     if not pattern.match(url):
         return {"error": "Invalid URL"}, 400
-    connection = sqlite3.connect(f'{get_base_folder_path()}/database.db')
-    cursor = connection.cursor()
-    cursor.execute(f'INSERT INTO shorts (link) VALUES (?)', (url,))
-    connection.commit()
-    connection.close()
-    return {'url': f'/short/{cursor.lastrowid}'}
+    with connection() as cursor:
+        cursor.execute(f'INSERT INTO shorts (link) VALUES (?)', (url,))
+        return {'url': f'/short/{cursor.lastrowid}'}
 
 @app.get('/short/<path:short_id>')
 def get_short(short_id: int):
@@ -36,23 +37,21 @@ def index():
     return index_data
 
 def _get_short(short_id: int):
-    connection = sqlite3.connect(f'{get_base_folder_path()}/database.db')
-    cursor = connection.cursor()
-    cursor.execute(f'SELECT link FROM shorts WHERE id = ?', (short_id,))
-    result = cursor.fetchone()
-    connection.close()
+    with connection() as cursor:
+        cursor.execute(f'SELECT link FROM shorts WHERE id = ?', (short_id,))
+        result = cursor.fetchone()
+    if result is None:
+        return {"error": "Short link not found"}, 404
     return result[0]
+
+
 @app.delete('/short/<path:short_id>')
 def delete_short(short_id: int):
-    connection = sqlite3.connect(f'{get_base_folder_path()}/database.db')
-    cursor = connection.cursor()
-    cursor.execute(f'DELETE FROM shorts WHERE id = ?', (short_id,))
-    # check if row was deleted
-    if cursor.rowcount == 0:
-        connection.close()
-        return {"error": "Short link not found"}, 404
-    connection.commit()
-    connection.close()
+    with connection() as cursor:
+        cursor.execute(f'DELETE FROM shorts WHERE id = ?', (short_id,))
+        # check if row was deleted
+        if cursor.rowcount == 0:
+            return {"error": "Short link not found"}, 404
     return {'success': True}
 
 if __name__ == '__main__':
--- backend/bootstrap.py
+++ backend/bootstrap.py
@@ -1,16 +1,27 @@
 import os
 import sqlite3
+from contextlib import contextmanager
 
 def get_base_folder_path():
     return os.path.dirname(os.path.abspath(__file__))
 
-def bootstrap():
+@contextmanager
+def connection():
     connection = sqlite3.connect(f'{get_base_folder_path()}/database.db')
     cursor = connection.cursor()
-    with open(f'{get_base_folder_path()}/bootstrap.sql', 'r') as file:
-        cursor.executescript(file.read())
-    connection.commit()
-    connection.close()
+    try:
+        yield cursor
+        connection.commit()
+    except Exception:
+        # Rollback in case there is any error
+        connection.rollback()
+    finally:
+        connection.close()
+
+def bootstrap():
+    with connection() as cursor:
+        with open(f'{get_base_folder_path()}/bootstrap.sql', 'r') as file:
+            cursor.executescript(file.read())
 
 if __name__ == '__main__':
     bootstrap()
