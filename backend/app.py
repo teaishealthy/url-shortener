1b97de0303d70ba8fca151d20a09335d3fe989db
teaishealthy <teaishealthy@protonmail.com>
2021-12-05 07:23:50 +0100

    format with black

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import re
 from flask import Flask, redirect
 from flask_cors import CORS
 
-if __name__ != '__main__':
+if __name__ != "__main__":
     from .bootstrap import get_base_folder_path, connection
 else:
     from bootstrap import get_base_folder_path, connection
@@ -12,47 +12,55 @@ else:
 app = Flask(__name__)
 CORS(app)
 
-pattern = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
-with open(f'{get_base_folder_path()}/../frontend/index.html', 'r') as f:
+pattern = re.compile(
+    "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+)
+with open(f"{get_base_folder_path()}/../frontend/index.html", "r") as f:
     index_data = f.read()
 
-@app.post('/short/<path:url>')
+
+@app.post("/short/<path:url>")
 def create_short(url: str):
     if not pattern.match(url):
         return {"error": "Invalid URL"}, 400
     with connection() as cursor:
-        cursor.execute(f'INSERT INTO shorts (link) VALUES (?)', (url,))
-        return {'url': f'/short/{cursor.lastrowid}'}
+        cursor.execute(f"INSERT INTO shorts (link) VALUES (?)", (url,))
+        return {"url": f"/short/{cursor.lastrowid}"}
 
-@app.get('/short/<path:short_id>')
+
+@app.get("/short/<path:short_id>")
 def get_short(short_id: int):
     return redirect(_get_short(short_id))
 
-@app.get('/expand/<path:short_id>/')
+
+@app.get("/expand/<path:short_id>/")
 def expand_short(short_id: int):
     return _get_short(short_id)
 
-@app.get('/')
+
+@app.get("/")
 def index():
     return index_data
 
+
 def _get_short(short_id: int):
     with connection() as cursor:
-        cursor.execute(f'SELECT link FROM shorts WHERE id = ?', (short_id,))
+        cursor.execute(f"SELECT link FROM shorts WHERE id = ?", (short_id,))
         result = cursor.fetchone()
     if result is None:
         return {"error": "Short link not found"}, 404
     return result[0]
 
 
-@app.delete('/short/<path:short_id>')
+@app.delete("/short/<path:short_id>")
 def delete_short(short_id: int):
     with connection() as cursor:
-        cursor.execute(f'DELETE FROM shorts WHERE id = ?', (short_id,))
+        cursor.execute(f"DELETE FROM shorts WHERE id = ?", (short_id,))
         # check if row was deleted
         if cursor.rowcount == 0:
             return {"error": "Short link not found"}, 404
-    return {'success': True}
+    return {"success": True}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- backend/bootstrap.py
+++ backend/bootstrap.py
@@ -2,12 +2,14 @@ import os
 import sqlite3
 from contextlib import contextmanager
 
+
 def get_base_folder_path():
     return os.path.dirname(os.path.abspath(__file__))
 
+
 @contextmanager
 def connection():
-    connection = sqlite3.connect(f'{get_base_folder_path()}/database.db')
+    connection = sqlite3.connect(f"{get_base_folder_path()}/database.db")
     cursor = connection.cursor()
     try:
         yield cursor
@@ -18,10 +20,12 @@ def connection():
     finally:
         connection.close()
 
+
 def bootstrap():
     with connection() as cursor:
-        with open(f'{get_base_folder_path()}/bootstrap.sql', 'r') as file:
+        with open(f"{get_base_folder_path()}/bootstrap.sql", "r") as file:
             cursor.executescript(file.read())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bootstrap()
